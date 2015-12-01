d6b53249bb2daeecb5309dc2d7e3ce23e20a1832
Diego Berrocal <cestdiego@gmail.com>
2015-12-01 17:14:57 -0500

    Revampt pop function

--- heap.py
+++ heap.py
@@ -52,9 +52,11 @@ class MaxHeap(object):
 
     def pop(self):
         value = self.peek()
-
-        self.array[0] = self.array.pop()
-        self.percolate_down(0)
+        if value is not None:
+            last = self.array.pop()
+            if not len(self.array) == 0:
+             self.array[0] = last
+             self.percolate_down(0)
         return value
 
     def populate(self, n):

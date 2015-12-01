c417bf66733bbaef6cde3079dfc918ec1548c3f9
Diego Berrocal <cestdiego@gmail.com>
2015-11-30 20:46:52 -0500

    Part 1.7

--- 1-Arrays-and-HashTables.py
+++ 1-Arrays-and-HashTables.py
@@ -20,7 +20,7 @@ def is_palyndrome(le_string):
         le_dict[char] += 1
 
     count = 0
-    for key, value in le_dict.iteritems(): if value % 2: count += 1
+    # for key, value in le_dict.iteritems(): if value % 2: count += 1
 
     if length % 2:
         ## if odd length, only one odd repetition of letter is allowed. 1, 3, 5, etc
@@ -67,3 +67,58 @@ def string_comprehension(string):
         le_string += c + str(n)
 
     return le_string
+
+
+
+coords = [(1,3), (1,2), (3,4)]
+#1.7
+def rotate_indices(coord, size):
+    i, j = coord
+    return j, size - 1 - i
+
+def get_all_rotated_indices(coord, size):
+    "Sorry not sorry."
+    first  = coord
+    second = rotate_indices(first, size)
+    third  = rotate_indices(second, size)
+    fourth = rotate_indices(third, size)
+
+    return [first, second, third, fourth]
+
+
+def switch_points(mat, coord1, coord2):
+    i1, j1 = coord1
+    i2, j2 = coord2
+
+    mat[i1][j1] , mat[i2][j2] = mat[i2][j2], mat[i1][j1]
+
+
+def rotate_points(mat, coords, size):
+    "Coords is an array of four indices that are rotation of the previous one"
+    val = [mat[i][j] for (i,j) in coords]
+
+    switch_points(mat, coords[0], coords[3])
+    switch_points(mat, coords[1], coords[3])
+    switch_points(mat, coords[2], coords[3])
+
+def doIt(mat, N):
+    for i in xrange(0, N/2):
+        for j in xrange(i, N - i - 1):
+            coords = get_all_rotated_indices((i,j), N)
+            rotate_points(mat, coords, N)
+    return mat
+
+def test():
+    mat = [[1,2,3],[4,5,6],[7,8,9]]
+    expected_mat = [[7,4,1],[8,5,2],[9,6,3]]
+    return expected_mat == doIt(mat, 3)
+
+def test2():
+    mat = [[1]]
+    expected_mat = [[1]]
+    return expected_mat == doIt(mat, 1)
+
+def test3():
+    mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+    expected_mat = [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]
+    return expected_mat == doIt(mat, 4)
